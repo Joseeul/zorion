c6e1d959c3159a45bb7d7cf35d11376158bad2f2
Joseeul <jose_andreas@icloud.com>
2025-10-10 15:39:20 +0700

    Initial commit

---
+++ zorion/zorionApp.swift
@@ -0,0 +1,17 @@
+//
+//  zorionApp.swift
+//  zorion
+//
+//  Created by Jose Andreas on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct zorionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
