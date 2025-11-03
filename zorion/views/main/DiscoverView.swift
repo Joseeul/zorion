6284ca467d3ec4eaf70821bfc152d5152ca091df
Joseeul <jose_andreas@icloud.com>
2025-11-03 12:15:02 +0700

    Discover screen layout

--- zorion/views/main/DiscoverView.swift
+++ zorion/views/main/DiscoverView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct DiscoverView: View {
+    @State private var searchQuery: String = ""
+    
     var body: some View {
-        Text("DISCOVER VIEW")
+        VStack(alignment: .leading) {
+            HStack {
+                TextField(
+                    "Search...",
+                    text: $searchQuery
+                )
+                .padding(.vertical, 12)
+                .padding(.horizontal, 8)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(
+                            Color.zorionGray,
+                            lineWidth: 0.5
+                        )
+                )
+                
+                Button(action: {}, label: {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 16, height: 16)
+                        .clipShape(.circle)
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                })
+                .padding(14)
+                .background(.zorionPrimary)
+                .clipShape(Circle())
+            }
+            
+            Text("Discover popular rooms")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top, 12)
+            
+            RoomHeader(imageUrl: "chat_blue", roomName: "Joseeul Chill Room", roomDesc: "Tempatnya ngechill sambil ngobrolin masa depan")
+            
+            RoomHeader(imageUrl: "chat_dark_blue", roomName: "Joseeul Chill Room", roomDesc: "Tempatnya ngechill sambil ngobrolin masa depan")
+            
+            RoomHeader(imageUrl: "chat_pink", roomName: "Joseeul Chill Room", roomDesc: "Tempatnya ngechill sambil ngobrolin masa depan")
+            
+            Spacer()
+        }
+        .padding()
     }
 }
 
