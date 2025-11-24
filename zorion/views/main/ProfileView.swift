f11a30bf6b2c318f80a7eec1f7cc63a1b0c19222
Jose <jose_andreas@icloud.com>
2025-11-25 03:04:45 +0700

    Profile view layout update

--- zorion/views/main/ProfileView.swift
+++ zorion/views/main/ProfileView.swift
@@ -58,68 +58,176 @@ struct ProfileView: View {
                 Text("Joined since \(user?.created_at.formatted(.dateTime.year()) ?? Date().formatted(.dateTime.year()))")
                     .font(.footnote)
                 
-                Text("Settings")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .padding(.vertical, 12)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                Button(action: {}, label: {
-                    Text("Edit profile")
-                    
-                    Spacer()
-                    
-                    Image(systemName: "chevron.right")
-                })
-                .frame(maxWidth: .infinity)
-                .padding([.top, .bottom], 12)
-                .padding([.trailing, .leading], 8)
-                .background(.zorionSettings)
-                .foregroundStyle(.zorionGray)
-                .cornerRadius(8)
-                .padding(.bottom, 8)
-                
-                Button(action: {}, label: {
-                    Text("Appearances")
-                    
-                    Spacer()
-                    
-                    Image(systemName: "chevron.right")
-                })
-                .frame(maxWidth: .infinity)
-                .padding([.top, .bottom], 12)
-                .padding([.trailing, .leading], 8)
-                .background(.zorionSettings)
-                .foregroundStyle(.zorionGray)
-                .cornerRadius(8)
-                
-                Spacer()
-                
-                Button(action: {
-                    Task {
-                        try await AuthController().signOutUser()
+                ScrollView {
+                    Group {
+                        Text("User Settings")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.vertical, 12)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Button(action: {}, label: {
+                            Text("Change username")
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        })
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom], 12)
+                        .padding([.trailing, .leading], 8)
+                        .background(.zorionSettings)
+                        .foregroundStyle(.zorionGray)
+                        .cornerRadius(8)
+                        .padding(.bottom, 8)
+                        
+                        Button(action: {}, label: {
+                            Text("Change profile picture")
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        })
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom], 12)
+                        .padding([.trailing, .leading], 8)
+                        .background(.zorionSettings)
+                        .foregroundStyle(.zorionGray)
+                        .cornerRadius(8)
+                        .padding(.bottom, 8)
+                        
+                        Button(action: {}, label: {
+                            Text("I'm a content creator")
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        })
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom], 12)
+                        .padding([.trailing, .leading], 8)
+                        .background(.zorionSettings)
+                        .foregroundStyle(.zorionGray)
+                        .cornerRadius(8)
+                        .padding(.bottom, 8)
                     }
-                    UserDefaults.standard.set(false, forKey: "isLogin")
-                    UserDefaults.standard.removeObject(forKey: "userEmail")
-                    UserDefaults.standard.removeObject(forKey: "userPassword")
-                    UserDefaults.standard.removeObject(forKey: "isContentCreator")
-                    UserDefaults.standard.removeObject(forKey: "userUsername")
-                    UserDefaults.standard.removeObject(forKey: "roomName")
-                    UserDefaults.standard.removeObject(forKey: "roomDesc")
-                    UserDefaults.standard.removeObject(forKey: "useOauth")
-                    UserDefaults.standard.removeObject(forKey: "userId")
-                }, label: {
-                    Text("LOGOUT")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                })
-                .frame(maxWidth: .infinity)
-                .padding([.top, .bottom], 12)
-                .padding([.trailing, .leading], 8)
-                .fontWeight(.semibold)
-                .background(.zorionPrimary)
-                .foregroundStyle(.white)
-                .cornerRadius(8)
+                    
+                    Group {
+                        Text("Creator Settings")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.vertical, 12)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Button(action: {}, label: {
+                            Text("Change room photo")
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        })
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom], 12)
+                        .padding([.trailing, .leading], 8)
+                        .background(.zorionSettings)
+                        .foregroundStyle(.zorionGray)
+                        .cornerRadius(8)
+                        .padding(.bottom, 8)
+                        
+                        Button(action: {}, label: {
+                            Text("Change room name")
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        })
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom], 12)
+                        .padding([.trailing, .leading], 8)
+                        .background(.zorionSettings)
+                        .foregroundStyle(.zorionGray)
+                        .cornerRadius(8)
+                        .padding(.bottom, 8)
+                        
+                        Button(action: {}, label: {
+                            Text("Change room description")
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        })
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom], 12)
+                        .padding([.trailing, .leading], 8)
+                        .background(.zorionSettings)
+                        .foregroundStyle(.zorionGray)
+                        .cornerRadius(8)
+                        .padding(.bottom, 8)
+                        
+                        Button(action: {}, label: {
+                            Text("Manage room members")
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        })
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom], 12)
+                        .padding([.trailing, .leading], 8)
+                        .background(.zorionSettings)
+                        .foregroundStyle(.zorionGray)
+                        .cornerRadius(8)
+                    }
+                    
+                    Group {
+                        Text("App Settings")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.vertical, 12)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Button(action: {}, label: {
+                            Text("Theme")
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        })
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom], 12)
+                        .padding([.trailing, .leading], 8)
+                        .background(.zorionSettings)
+                        .foregroundStyle(.zorionGray)
+                        .cornerRadius(8)
+                        .padding(.bottom, 8)    
+                    }
+                    
+                    Button(action: {
+                        Task {
+                            try await AuthController().signOutUser()
+                        }
+                        UserDefaults.standard.set(false, forKey: "isLogin")
+                        UserDefaults.standard.removeObject(forKey: "userEmail")
+                        UserDefaults.standard.removeObject(forKey: "userPassword")
+                        UserDefaults.standard.removeObject(forKey: "isContentCreator")
+                        UserDefaults.standard.removeObject(forKey: "userUsername")
+                        UserDefaults.standard.removeObject(forKey: "roomName")
+                        UserDefaults.standard.removeObject(forKey: "roomDesc")
+                        UserDefaults.standard.removeObject(forKey: "useOauth")
+                        UserDefaults.standard.removeObject(forKey: "userId")
+                    }, label: {
+                        Text("LOGOUT")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .padding([.top, .bottom], 12)
+                    .padding([.trailing, .leading], 8)
+                    .fontWeight(.semibold)
+                    .background(.zorionPrimary)
+                    .foregroundStyle(.white)
+                    .cornerRadius(8)
+                }
             }
         }
         .padding()
